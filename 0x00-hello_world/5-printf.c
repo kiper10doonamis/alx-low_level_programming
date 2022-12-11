5086db5f1f14480850927307f7bebe515ff59ef6
kiper10doonamis <kiprehermann29@gmail.com>
2022-12-11 14:03:50 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/* stdio.h allows to display text on the screen*/
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 if successuf
+ */
+
+int main(void)
+
+{
+	printf("with proper grammar,but the outcome is a piece of art,\n");
+	return (0);
+}
