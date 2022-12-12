695db0b12b9d7c431caa08a0c98916257fd81c69
kiper10doonamis <kiprehermann29@gmail.com>
2022-12-11 23:41:09 -0800

    intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return : Always 1 (sucess)
+ */
+
+int main(void)
+
+{
+	fprint(stderr, "and that piece of art is ueful\"-\ Dora Korpar,2015-10-19\n");
+
+	return (1);
+}
