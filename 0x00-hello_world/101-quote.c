0a8b36b1773f11666354a87d9f06e68738cce014
kiper10doonamis <kiprehermann29@gmail.com>
2022-12-13 10:48:21 -0800

    description

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Return : Always 1 (sucess)
+ * Return: Always 1 (sucess)
  */
 
 int main(void)
 
 {
-	fprint(stderr, "and that piece of art is ueful\"-\ Dora Korpar,2015-10-19\n");
+	write(2, "and that piece of art is ueful\"-Dora Korpar,2015-10-19\n", 59);
 
 	return (1);
 }
